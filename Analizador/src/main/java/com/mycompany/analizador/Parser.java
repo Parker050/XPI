21a4ca89041da9e6b26c2f8ec2b790b2b7a44e7d
d4ni3la <149011696+d4ni3la@users.noreply.github.com>
2024-12-03 23:54:26 -0600

    Update Parser.java

--- Analizador/src/main/java/com/mycompany/analizador/Parser.java
+++ Analizador/src/main/java/com/mycompany/analizador/Parser.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.analizador;
 
 import java.util.List;
 
-/**
- *
- * @author danielaperez
- */
 public class Parser {
     private List<Token> tokens;
     private int index;
